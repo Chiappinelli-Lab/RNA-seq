35db665a7f49027058a8245541d993d0e7bee617
grundye <40005481+grundye@users.noreply.github.com>
2020-06-04 12:23:26 -0400

    replace line 248 that got removed

--- RNAseq_snakemake_pipeline/make.TCGA.telescope.DESeq2.tibble.py
+++ RNAseq_snakemake_pipeline/make.TCGA.telescope.DESeq2.tibble.py
@@ -245,6 +245,8 @@ def make_graphs(tibble_file, summary_data_name):
         DE_trx_col = 2
         print("WARNING!! Using column index {index} to determine how many genes were upregulated, downregulated, or had no change in expression from tibble file {file}".format(index= DE_trx_col, file = tibble_file))
 
+    DE_transcripts = tibble.iloc[:, [DE_trx_col, 1]] # dataframe with column 1 = UP, DOWN, or no_change and column 2 = log2fc values
+        
     # Get number of differentially expressed transcripts, number of upregulated transcripts, and number of downregulated transcripts
     n_total_DE_transcripts = len(DE_transcripts[DE_transcripts.iloc[:,0].isin(['UP', 'DOWN'])])
     n_upreg = len(DE_transcripts[DE_transcripts.iloc[:,0].isin(['UP'])])
